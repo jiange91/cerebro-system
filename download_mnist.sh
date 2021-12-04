7d8efe86abe690a77431adbec661e3547ca088c7
Zijian Jim <zijianjim@outlook.com>
2021-12-03 18:54:11 -0800

    download data to nfs

--- download_mnist.sh
+++ download_mnist.sh
@@ -3,4 +3,4 @@
 wget -q -O /tmp/gdrive.tar.gz "https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz"
 mkdir /tmp/gdrive && tar -xf /tmp/gdrive.tar.gz -C /tmp/gdrive
 chmod +x /tmp/gdrive/gdrive
-/tmp/gdrive/gdrive download 1RC7_jjJv0AcXsulnA5LNpDr_alHfCBaF --path ~/cerebro-system/data
+/tmp/gdrive/gdrive download 1RC7_jjJv0AcXsulnA5LNpDr_alHfCBaF --path /var/nfs/data
