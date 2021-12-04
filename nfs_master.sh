c5eb8ac54cfd62a8f7da89f0e3e40ff38eace225
Zijian Jim <zijianjim@outlook.com>
2021-12-03 16:22:18 -0800

    fix nfs mater

--- nfs_master.sh
+++ nfs_master.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get install nfs-kernel-server
 sudo mkdir /var/nfs
 sudo chown nobody:nogroup /var/nfs
-sudo echo "/var/nfs $master_ip (rw,sync,no_subtree_check,no_,no_root_squash)" | tee -a /etc/exports
+sudo echo "/var/nfs $master_ip (rw,sync,no_subtree_check,no_root_squash)" | tee -a /etc/exports
 sudo exportfs -a
 sudo service nfs-kernel-server start 
